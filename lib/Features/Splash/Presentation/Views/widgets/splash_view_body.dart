5e10d2f87c5fe5d984109560da28978af15afd17
mariamattef <121569095+mariamattef@users.noreply.github.com>
2024-10-13 07:47:41 +0300

    refactor init stete & Navigate home

--- lib/Features/Splash/Presentation/Views/widgets/splash_view_body.dart
+++ lib/Features/Splash/Presentation/Views/widgets/splash_view_body.dart
@@ -1,8 +1,33 @@
 import 'package:booklyapp/Core/utils/assets.dart';
+import 'package:booklyapp/Features/Splash/Presentation/Views/widgets/sliding_text.dart';
+import 'package:booklyapp/Features/home/presentation/views/pages/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationColnroller;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    _animationColnroller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +39,25 @@ class SplashViewBody extends StatelessWidget {
         const SizedBox(
           height: 5,
         ),
-        const Text('Read Free Books', textAlign: TextAlign.center),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: kTabScrollDuration);
+    });
+  }
+
+  void initSlidingAnimation() {
+    _animationColnroller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: Offset.zero,
+    ).animate(_animationColnroller);
+    _animationColnroller.forward();
   }
 }
